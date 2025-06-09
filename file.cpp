a4b4af493be26db8cb299527cac8e7016b6cbec8
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-06-09 18:14:36 +0700

    chose iput file

--- file.cpp
+++ file.cpp
@@ -1,39 +1,8 @@
-#include <iostream>
-#include <fstream>  // Needed for ofstream
-#include <string>
-using namespace std;
 
-int main()
-{
-    string baris;
-    ofstream outfile;
-
-    outfile.open("contohfile.txt");
-
-    cout << ">= Menulis file, 'q' untuk keluar" << endl;
-
-    while (true)
-    {
-        cout << "Masukan baris ";
-        getline(cin, baris);
-
-        if (baris == "q")
-            break;
-
-        outfile << baris << endl;  // Corrected output syntax
+        }
+        infile.close();
+        
     }
-
-    outfile.close();  // Always good practice to close the file
-    
-    ifstream infile;
-
-    infile.open("contohfile.text");
-
-    cout<<endl <<">=  Membuka dan membaca file "<<endl;
-
-    if (infile.is_open())
-    {
-        while (getline(infile, baris))
-        {
-            cout<< baris <<endl;
-       
+    else
+        cout<<"Unable to open file";
+}
