cb8a9e762ce0f0f5087440332881b27f09e37150
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-06-09 19:13:07 +0700

    alcomplete program

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
 
+int main()
+{
+    cout << "Awal program" << endl;
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        cout << data.at(4) << endl;
+    }
+    catch (exception &e)
+    {
         cout << e.what() << endl;
     }
     cout << "Baris program yang terakhir" << endl;
